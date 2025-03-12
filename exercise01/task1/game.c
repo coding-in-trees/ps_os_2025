824233494b25fe4e0c6fcd180e095f35bd4c8462
coding-in-trees <jimtanusch@web.de>
2025-03-12 12:48:19 +0100

    formatted game.c

--- exercise01/task1/game.c
+++ exercise01/task1/game.c
@@ -6,7 +6,7 @@ void printUsage(const char* programName) {
 	printf("usage: %s <width> <height> <density> <steps>\n", programName);
 }
 
-int**generate_empty_field(int width, int height);
+int** generate_empty_field(int width, int height);
 void init_field(int** field, int width, int height, double density);
 void free_field(int** field, int width);
 int generate_PBM(const int** field, int width, int height, int step);
@@ -15,7 +15,7 @@ int alive_neighbors(const int** field, int x, int y, int width, int height);
 int is_out_of_bounds(int x, int y, int width, int height);
 
 int main(int argc, char* argv[]) {
-	if(argc != 5) {
+	if (argc != 5) {
 		printUsage(argv[0]);
 		return EXIT_FAILURE;
 	}
@@ -30,8 +30,7 @@ int main(int argc, char* argv[]) {
 	printf("density: %4.0f%%\n", density * 100);
 	printf("steps:   %4d\n", steps);
 
-	if (steps < 0)
-	{
+	if (steps < 0) {
 		return EXIT_FAILURE;
 	}
 
@@ -48,19 +47,17 @@ int main(int argc, char* argv[]) {
 		return exit;
 	}
 
-	for (int i = 1; i < steps; i++)
-	{
+	for (int i = 1; i < steps; i++) {
 		simulate((const int**)source, destination, width, height);
 		int exit = generate_PBM((const int**)destination, width, height, i);
-		if (exit == EXIT_FAILURE)
-		{
+		if (exit == EXIT_FAILURE) {
 			return EXIT_FAILURE;
 		}
 		int** temp = source;
 		source = destination;
 		destination = temp;
 	}
-	
+
 	free_field(source, width);
 	free_field(destination, width);
 	return EXIT_SUCCESS;
@@ -68,26 +65,22 @@ int main(int argc, char* argv[]) {
 
 int** generate_empty_field(int width, int height) {
 	int** field = malloc(sizeof(int*) * width);
-	for (int i = 0; i < width; i++)
-	{
+	for (int i = 0; i < width; i++) {
 		field[i] = calloc(height, sizeof(int));
 	}
 	return field;
 }
 
 void free_field(int** field, int width) {
-	for (int i = 0; i < width; i++)
-	{
+	for (int i = 0; i < width; i++) {
 		free(field[i]);
 	}
 	free(field);
 }
 
-void init_field(int** field,int width, int height, double density) {
-	for (int i = 0; i < width; i++)
-	{
-		for (int j = 0; j < height; j++)
-		{
+void init_field(int** field, int width, int height, double density) {
+	for (int i = 0; i < width; i++) {
+		for (int j = 0; j < height; j++) {
 			double can_live = rand() / (double)RAND_MAX;
 			field[i][j] = can_live < density;
 		}
@@ -95,20 +88,17 @@ void init_field(int** field,int width, int height, double density) {
 }
 
 int generate_PBM(const int** field, int width, int height, int step) {
-    char fileName[14];  // Fixed size (gol_00000.pbm = 14 chars)
-    int size = snprintf(fileName, sizeof(fileName), "gol_%05d.pbm", step);
-	if (size < 0)
-	{
+	char fileName[14]; // Fixed size (gol_00000.pbm = 14 chars)
+	int size = snprintf(fileName, sizeof(fileName), "gol_%05d.pbm", step);
+	if (size < 0) {
 		return EXIT_FAILURE;
 	}
-	FILE *pbm = fopen(fileName, "w");
+	FILE* pbm = fopen(fileName, "w");
 
 	fprintf(pbm, "P1\n");
 	fprintf(pbm, "%d %d\n", width, height);
-	for (int i = 0; i < width; i++)
-	{
-		for (int j = 0; j < height; j++)
-		{
+	for (int i = 0; i < width; i++) {
+		for (int j = 0; j < height; j++) {
 			fprintf(pbm, "%d", !field[i][j]);
 		}
 	}
@@ -118,41 +108,30 @@ int generate_PBM(const int** field, int width, int height, int step) {
 }
 
 void simulate(const int** current, int** dest, int width, int height) {
-	for (int x = 0; x < width; x++)
-	{
-		for (int y = 0; y < height; y++)
-		{
+	for (int x = 0; x < width; x++) {
+		for (int y = 0; y < height; y++) {
 			int isAlive = current[x][y];
 			int countAlive = alive_neighbors(current, x, y, width, height);
-			if (!isAlive && countAlive == 3)
-			{
+			if (!isAlive && countAlive == 3) {
 				dest[x][y] = 1;
 				continue;
 			}
-			if (isAlive && (countAlive < 2 || countAlive > 3))
-			{
+			if (isAlive && (countAlive < 2 || countAlive > 3)) {
 				dest[x][y] = 0;
 				continue;
 			}
 			dest[x][y] = current[x][y];
 		}
 	}
-	
 }
 
 int alive_neighbors(const int** field, int x, int y, int width, int height) {
 	int sum = 0;
-	for (int pad_x = -1; pad_x < 2; pad_x++)
-	{
-		for (int pad_y = -1; pad_y < 2; pad_y++)
-		{
-			int curr_x = x+pad_x;
-			int curr_y = y+pad_y;
-			if (
-				(curr_x == x && curr_y == y)
-				|| is_out_of_bounds(curr_x, curr_y, width, height)
-			)
-			{
+	for (int pad_x = -1; pad_x < 2; pad_x++) {
+		for (int pad_y = -1; pad_y < 2; pad_y++) {
+			int curr_x = x + pad_x;
+			int curr_y = y + pad_y;
+			if ((curr_x == x && curr_y == y) || is_out_of_bounds(curr_x, curr_y, width, height)) {
 				continue;
 			}
 			sum += field[curr_x][curr_y];
